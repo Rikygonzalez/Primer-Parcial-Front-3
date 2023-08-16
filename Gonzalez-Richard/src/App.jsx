ed456102d116bd52d2f02f424870c9749137d740
Richard González <rikygonzalez09@gmail.com>
2023-08-16 17:38:00 -0300

    Se agrego el contenido la clase 10 del profesor

--- Gonzalez-Richard/src/App.jsx
+++ Gonzalez-Richard/src/App.jsx
@@ -1,34 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Menu } from './Componentes/Menu'
+import { Pedido } from './Componentes/Pedido'
+import { getId } from './utils/getId'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [pedidos, setPedidos] = useState([])
+
+  function agregarAlPedido(item) {
+    const newPedido = {
+      id: getId(),
+      item,
+    }
+    setPedidos([...pedidos, newPedido])
+  }
+
+  function eliminarPedido(id) {
+    const nuevoPedido = pedidos.filter((pedido) => pedido.id !== id)
+    setPedidos(nuevoPedido)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='flex flex-column'>
+      <Menu agregarAlPedido={agregarAlPedido} />
+      <ul>
+        {pedidos.map((elemPedido) => {
+          return (
+            <Pedido
+              key={elemPedido.id}
+              pedido={elemPedido.item}
+              eliminarPedido={() => eliminarPedido(elemPedido.id)}
+            />
+          )
+        })}
+      </ul>
+    </div>
   )
 }
 
